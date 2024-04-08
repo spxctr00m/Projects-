48b5e5db34a787b017c3f03c873d64c41f44d549
spxctr00m <bramainestone@gmail.com>
2024-04-08 09:37:13 +0100

    I added a click and hold feature in the javascript for an easier experience

--- Simple Counter App/app.js
+++ Simple Counter App/app.js
@@ -16,3 +16,41 @@ resetBtn.onclick = function(){
     count = 0;
     countLabel.textContent = count;
 }
+const clickAndHold = (btnEl) => {
+    let timerId;
+    const DURATION = 200;
+  
+    //handle when clicking down
+    const onMouseDown = () => {
+      timerId = setInterval(() => {
+        btnEl && btnEl.click();
+      }, DURATION);
+    };
+  
+    //stop or clear interval
+    const clearTimer = () => {
+      timerId && clearInterval(timerId);
+    };
+  
+    //handle when mouse is clicked
+    btnEl.addEventListener("mousedown", onMouseDown);
+    //handle when mouse is raised
+    btnEl.addEventListener("mouseup", clearTimer);
+    //handle mouse leaving the clicked button
+    btnEl.addEventListener("mouseout", clearTimer);
+  
+    // a callback function to remove listeners useful in libs like react
+    // when component or element is unmounted
+    return () => {
+      btnEl.removeEventListener("mousedown", onMouseDown);
+      btnEl.removeEventListener("mouseup", clearTimer);
+      btnEl.removeEventListener("mouseout", clearTimer);
+    };
+  };
+  
+  
+  //onMount
+  document.addEventListener("DOMContentLoaded", function () {
+    clickAndHold(increaseBtn);
+    clickAndHold(decreaseBtn);
+  });
