a71e5ed53e7b936ac3f0805aaf47316ce507d77a
Bramstone <en4abraham@gmail.com>
2024-07-21 07:33:54 +0100

    Nothing to write here

---
+++ Weather App/app.js
@@ -0,0 +1,26 @@
+const weatherForm = document.querySelector(".weatherForm")
+const cityInput = document.querySelector(".cityInput")
+const card = document.querySelector(".card")
+const apiKey = "55cb39d6b2106f91a641ddbc07d1ab6b";
+
+weatherForm.addEventListener('submit', event => {
+
+event.prevevevvee
+
+})
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data) {
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+
+}
